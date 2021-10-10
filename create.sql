08193f5d3ea5ab198cb002730ab8eeef6e3ac2f5
Oliveryql <49108241+Oliveryql@users.noreply.github.com>
2021-10-10 13:58:41 -0500

    Add files via upload
    Initial commit

---
+++ create.sql
@@ -0,0 +1,45 @@
+drop table if exists Item;
+
+drop table if exists Auction;
+
+drop table if exists User;
+
+drop table if exists Category;
+
+create table Item (
+    ItemID CHAR(10) PRIMARY KEY,
+    Name VARCHAR,
+    Currently CHAR(10),
+    Buy_Price CHAR(10),
+    First_Bid CHAR(10),
+    Number_of_Bids CHAR(5),
+    Started CHAR(20),
+    Ends CHAR(20),
+    Seller_UserID VARCHAR,
+    Description TEXT
+);
+
+create table User(
+    UserID VARCHAR PRIMARY KEY,
+    Location VARCHAR,
+    Country VARCHAR,
+    Rating VARCHAR
+);
+
+create table Auction(
+    ItemID CHAR(10),
+    UserID VARCHAR,
+    Time VARCHAR,
+    Amount VARCHAR,
+    PRIMARY KEY(ItemID,UserID,Time)
+    FOREIGN KEY (ItemID)
+        REFERENCES Item(ItemID),
+    FOREIGN KEY (UserID)
+        REFERENCES User(UserID)
+);
+
+create table Category(
+    ItemID CHAR(10),
+    Category VARCHAR,
+    PRIMARY KEY(ItemID,Category)
+);
---
+++ runParser.sh
@@ -0,0 +1,5 @@
+python my_parser.py ebay_data/items-*.json
+sort item.dat | uniq
+sort user.dat | uniq
+sort auction.dat | uniq
+sort category.dat | uniq
